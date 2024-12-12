f1284565419f225edef0925c0b2187d381f34500
yevheniia.zhygalova <YevaITUA@gmail.com>
2024-12-12 14:11:09 +0100

    Lesson 5

--- Kaizen.Ng/src/app/app.component.ts
+++ Kaizen.Ng/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { User } from '../models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.sass']
 })
-
 export class AppComponent {
-  users : User[] = [{id : 1, email : "email@email", "password" : "password"}];
   title = 'kaizen';
 }
